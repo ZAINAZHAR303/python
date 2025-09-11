7e08e4db80b0aef74696d57ef30ab041301345c8
ZAINAZHAR303 <zainazhar303@gmail.com>
2025-09-11 23:59:19 +0500

    add the php file

---
+++ PHP web Class/index - Copy.php
@@ -0,0 +1,17 @@
+
+
+<?php 
+
+    // defining objeact in PHP
+    define("App_User", "code with zain" );
+    // <h1>hello worls</h1>
+    echo "hello world";
+    echo "hellooo world! ";
+
+    $brands = array('apple','microsoft','google');
+    
+    print_r($brands);
+
+    // getting object in php
+    print_r(value:App_User);
+?>
